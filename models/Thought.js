932b2eee3d0cbb5a428ee870043f6c362844230f
deand925 <97762768+deand925@users.noreply.github.com>
2022-11-08 12:34:44 -0800

    added models

---
+++ models/Thought.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: reactionId._id,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => {
+            if (date) return date.toISOString().split("T") [0];
+          },
+    }
+})
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => {
+            if (date) return date.toISOString().split("T") [0];
+          },
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+})
